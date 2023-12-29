739191c0f3a6452711848d6a05e4c3510f9547f2
Divyansh Gupta <123203281+divyansh12git@users.noreply.github.com>
2023-12-29 23:38:04 +0530

    basic structure completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import Nav from "./components/Nav";
+import Jumbotron from "./components/Jumbotron";
+import SoundSection from "./components/SoundSection";
+import DisplaySection from "./components/DisplaySection";
 
 function App() {
 
   return (
+    <>
+    
     <div className="App">
-      Hello
+      <Nav />
+      <Jumbotron />
+      <SoundSection />
+      <DisplaySection />
     </div>
+    </>
   );
 }
 
